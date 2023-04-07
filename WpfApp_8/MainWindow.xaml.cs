272b280cc69099dff6c13365175f0b39e294bc25
heliconia8 <d.samokvasova@gmail.com>
2023-04-07 10:26:12 +0400

    Добавьте файлы проекта.

---
+++ WpfApp_8/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox
+                .Show("Вы действительно хотите удалить этот файл?", "внимание!",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("товар удален!");
+            }
+            else
+            {
+                MessageBox.Show("товар не удален!");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox
+                .Show("Добавить товар", "внимание!",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("товвар добавлен!");
+            }
+            else
+            {
+                MessageBox.Show("товар не добавлен!");
+            }
+        }
+
+        private void tb_generate_Click(object sender, RoutedEventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new();
+            QRCodeData qRCodeData = qrGenerator.CreateQrCode(tb_content.Text, QRCodeGenerator.ECCLevel.Q);
+
+            QRCode qR = new QRCode(qRCodeData);
+
+            Bitmap qr = qR.GetGraphic(150);
+
+            image_qrcode.Source = Convert(qr);
+
+        }
+        public BitmapImage Convert(Bitmap src)
+        {
+            MemoryStream ms = new MemoryStream();
+            ((System.Drawing.Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+    }
+}
